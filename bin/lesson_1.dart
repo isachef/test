92cb7709c023588d65312c2ab2ab753e6b8bd2dd
isas <isa.smailbekov@ctechnology.kg>
2024-09-21 18:43:09 +0600

    first commit

---
+++ bin/lesson_1.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:lesson_1/lesson_1.dart';
+
+void main(List<String> arguments) async {
+  // print('lop1');
+  // await Future.delayed(Duration(seconds: 2));
+  // print('lop2');
+  // delay();
+
+  print(await sortB([5, 4, 1, 3, 9, 1, 2]));
+}
+
+Future<List<int>> sortB(List<int> array) async {
+  await Future.delayed(Duration(seconds: 2));
+  List<int> arraySorted = [];
+  for (var i = array.length-1; i >= 0; i--) {
+    int min = array.elementAt(i);
+    int index = i;
+
+    for (int ii = array.length -1; ii >= 0; ii--) {
+      if (array.elementAt(ii) < min) {
+        min = array.elementAt(ii);
+        index = ii;
+      }
+    }
+    
+    arraySorted.add(min);
+    array.removeAt(index);
+  }
+  return arraySorted;
+}
+
+Future<List<int>> sort(List<int> array) async {
+  await Future.delayed(Duration(seconds: 2));
+  for (var i = 0; i < array.length; i++) {
+    int min = array.elementAt(i);
+    int index = i;
+    for (int ii = i; ii < array.length; ii++) {
+      if (array.elementAt(ii) < min) {
+        min = array.elementAt(ii);
+        index = ii;
+      }
+    }
+    array[index] = array[i];
+    array[i] = min;
+  }
+  return array;
+}
+
+void sequence(int next, int last) {
+  if (next == last) {
+    print(next);
+  } else {
+    print(next);
+    sequence(next + 1, last);
+  }
+}
+
+void recurce(int n) {
+  if (n > 1) {
+    recurce(n - 1);
+  }
+  print(n);
+}
---
+++ lib/lesson_1.dart
@@ -0,0 +1,14 @@
+int calculate() {
+  return 6 * 7;
+}
+
+
+hello() {
+  print('hello world');
+}
+
+int sumator(int a, int b, String pop, num asd) {
+  return a + b;
+}
+
+String isOdd(int num) => num % 2 == 0 ? 'yes' : 'no';
