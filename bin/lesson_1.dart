5e9568f64db57f281ce5f97553ddf02b06103b68
isas <isa.smailbekov@ctechnology.kg>
2024-09-21 20:18:53 +0600

    disarium

--- bin/lesson_1.dart
+++ bin/lesson_1.dart
@@ -10,23 +10,36 @@ void main(List<String> arguments) async {
   // print('lop2');
   // delay();
 
-  print(await sortB([5, 4, 1, 3, 9, 1, 2]));
+  // print(await sortB([5, 4, 1, 3, 9, 1, 2]));
+  print(disarium(135));
+}
+
+String disarium(int n) {
+  int length = n.toString().length;
+  int sum = 0;
+  int nn = n;
+  for (var i = 1; i <= length; i++) {
+    int expon = (pow(10, length - i)).toInt();
+    sum += pow(nn ~/ expon, i).toInt();
+    nn %= expon;
+  }
+  return n == sum ? 'yes' : 'no';
 }
 
 Future<List<int>> sortB(List<int> array) async {
   await Future.delayed(Duration(seconds: 2));
   List<int> arraySorted = [];
-  for (var i = array.length-1; i >= 0; i--) {
+  for (var i = array.length - 1; i >= 0; i--) {
     int min = array.elementAt(i);
     int index = i;
 
-    for (int ii = array.length -1; ii >= 0; ii--) {
+    for (int ii = array.length - 1; ii >= 0; ii--) {
       if (array.elementAt(ii) < min) {
         min = array.elementAt(ii);
         index = ii;
       }
     }
-    
+
     arraySorted.add(min);
     array.removeAt(index);
   }
